6865dd92b739a01e078dd9ac24f8fe48c5e29ac8
quiquep2003 <quiquep2003@gmail.com>
2025-08-26 19:43:27 -0300

    agrego fichero tex y hist20250826.sh

---
+++ hist20250826.sh
@@ -0,0 +1,24 @@
+u0_a349@termux:~/agenda/$ ls
+README.md  agenda099.c  gnd2
+u0_a349@termux:~/agenda/$ nano agenda099.cu0_a349@termux:~/agenda/$ gcc agenda099.c -o agenda
+u0_a349@termux:~/agenda/$ ./agenda
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 1
+Ingrese un nombre a buscar: qui
+61 quique 15-7007-0601
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 0
+Salimos!
+u0_a349@termux:~/agenda/$
