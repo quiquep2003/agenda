ded54c940c3f73dd7cfdde591e79cb76f4495333
quiquep2003 <quiquep2003@gmail.com>
2025-09-09 14:08:56 -0300

    modifico gnd2 y asiento comportamiento de agenda099.c

---
+++ hist20250906.sh
@@ -0,0 +1,12 @@
+  500  cs agenda
+  501  nano text20250905.txt
+  502  git status
+  503  git add text20250905.txt
+  504  git status
+  505  git commit -m "corrección del texto"
+  506  git log
+  507  ls
+  508  git log --oneline
+  509  git diff 7b368a3 b4ef1e7
+  510  ls
+  511  history > hist20250906.sh
