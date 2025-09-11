3db6a17fb146376718f66ead120777fedaf98829
quiquep2003 <quiquep2003@gmail.com>
2025-09-11 02:39:38 -0300

    añado la funcion split

---
+++ hist20250911.sh
@@ -0,0 +1,127 @@
+u0_a349@termux:~/.../fs-mgnt03/agenda/$ ./agenda100
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 2
+
+* Agenda de Contactos *
+1 aldo 15-8454-8598
+2 alfonso 15-7978-6453
+3 alicia 15-5960-9584
+4 alonso 15-3202-1660
+5 amanda 15-8488-0867
+6 anabella 15-9594-8409
+7 andrea 15-3691-2361
+8 andrea 15-4929-5970
+9 andres 15-7852-8483
+10 antonio 15-3456-3231
+11 camila 15-3442-8483
+12 carina 15-9876-4356
+13 carla 15-3456-9876
+14 carlos 15-3043-9081
+15 cecilia 15-9631-8421
+16 celedonio 15-9342-9980
+17 celia 15-4020-1314
+18 cesar 15-4020-1603
+19 cintia 15-1235-8132
+20 claudia 15-9283-8374
+21 cristian 15-8832-7655
+22 daniela 15-8098-7030
+23 diego 15-1010-5435
+24 dru 15-4213-7524
+25 eduardo 15-6732-7164
+26 eugenia 15-0232-0911
+27 ezequiel 15-9219-5646
+28 felicia 15-3939-5482
+29 felipe 15-8994-4003
+30 fernanda 15-0230-4432
+31 flavia 15-6474-4487
+32 florencia 15-4423-4839
+33 gimena 15-1248-1236
+34 gustavo 15-5439-6420
+35 hector 15-9483-8372
+36 ines 15-0000-1112
+37 jaime 15-1928-4239
+38 javier 15-3267-5496
+39 jorge 15-3442-8230
+40 josefina 15-6367-4466
+41 juan carlos 15-4543-3355
+42 juan pablo 15-3256-3665
+43 julia 15-1235-4771
+44 julian 15-6734-7167
+45 kalí 15-2522-9315
+46 laura 15-1111-2222
+47 leandro 15-9643-6432
+48 lorenzo 15-9432-9635
+49 luciano 15-3241-5896
+50 luis 15-0885-7039
+51 marcela 15-7793-8958
+52 marcos 15-8833-8935
+53 mario 15-6248-9904
+54 micaela 15-2346-2591
+55 natalia 15-2348-5791
+56 nicolas 15-8296-5759
+57 octavio 15-9654-0757
+58 ortencio 15-4234-2640
+59 osvaldo 15-3646-6867
+60 pablo 15-4152-1937
+61 pamela 15-6321-8421
+62 patricio 15-8485-7877
+63 quique 15-7007-0601
+64 rafael 15-8839-7487
+65 ramiro 15-4432-3422
+66 raul 15-3452-3520
+67 reinaldo 15-5938-3377
+68 rodrigo 15-1010-paco
+69 romina 15-3345-8766
+70 romina 15-8383-6562
+71 ruben 15-6639-3772
+72 rubicio 15-1630-2315
+73 sabrina 15-2896-1359
+74 samuel 15-8499-4486
+75 saul 15-2928-8987
+76 sebastian 15-4258-9327
+77 sergio 15-9832-5426
+78 silvana 15-4699-7578
+79 silvio 15-2829-2844
+80 tatiana 15-3343-4592
+81 tulio 15-9706-3794
+82 zamuel 15-9032-1201
+83 zulema 15-3296-1395
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 1
+Ingrese un nombre a buscar: pablo
+42 juan pablo 15-3256-3665
+60 pablo 15-4152-1937
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 1
+Ingrese un nombre a buscar: carlos
+14 carlos 15-3043-9081
+41 juan carlos 15-4543-3355
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 0
+Salimos!
+u0_a349@termux:~/.../fs-mgnt03/agenda/$
+
