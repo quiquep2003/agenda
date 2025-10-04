feaff984cc6d5ece3ec344420aefb44661e60199
quiquep2003 <quiquep2003@gmail.com>
2025-10-04 12:09:31 -0300

    añadido de submenu de busqueda

---
+++ hist20251004.sh
@@ -0,0 +1,74 @@
+u0_a349@termux:~/agenda/$ ./agenda
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 1
+Ingrese un nombre a buscar: j
+38 jaime 15-1928-4239
+39 javier 15-3267-5496
+40 jorge 15-3442-8230
+41 jose luis alberto 15-4545-2498
+42 josefina 15-6367-4466
+43 juan carlos 15-4543-3355
+44 juan luis enrique 15-9796-8586
+45 juan pablo 15-3256-3665
+46 julia 15-1235-4771
+47 julian 15-6734-7167
+
+* busqueda adicional *
+Afina.................: 1
+Volver................: 0
+Opción................: 1
+Ingrese el siguiente texto a buscar: lu
+41 jose luis alberto 15-4545-2498
+44 juan luis enrique 15-9796-8586
+
+* busqueda adicional *
+Afina.................: 1
+Volver................: 0
+Opción................: 1
+Ingrese el siguiente texto a buscar: en
+44 juan luis enrique 15-9796-8586
+
+* busqueda adicional *
+Afina.................: 1
+Volver................: 0
+Opción................: 0
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 1
+Ingrese un nombre a buscar: juan
+43 juan carlos 15-4543-3355
+44 juan luis enrique 15-9796-8586
+45 juan pablo 15-3256-3665
+
+* busqueda adicional *
+Afina.................: 1
+Volver................: 0
+Opción................: 1
+Ingrese el siguiente texto a buscar: carlos
+43 juan carlos 15-4543-3355
+
+* busqueda adicional *
+Afina.................: 1
+Volver................: 0
+Opción................: 0
+
+* Agenda de Contactos *
+Salir.................: 0
+Buscar................: 1
+Listar................: 2
+Nuevo.................: 3
+Borrar................: 4
+Opcion................: 0
+Salimos!
+u0_a349@termux:~/agenda/$
